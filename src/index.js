a91c8fd5466ad67cc563d9fb0d81ed1f224d770b
NatUsik6 <tashausik6@gmail.com>
2023-02-19 19:31:22 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (str.length != 0) {
+    let failCount = 0;
+    bracketsConfig.forEach(config => {
+      let index = str.indexOf(config[0] + config[1]);
+      if (index != -1) {
+        str = str.slice(0, index) + str.slice(index + 2);
+      } else {
+        failCount++;
+      }
+    });
+    
+    if (failCount == bracketsConfig.length) {
+      return false;
+    }
+  }
+
+  return true;
 }
